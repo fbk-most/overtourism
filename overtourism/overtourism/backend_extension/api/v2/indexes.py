17106b0c55082223da26674b1f1ce66b84652671
aricci <aricci@fbk.eu>
2026-07-29 15:51:28 +0200

    add dealing with map geojson IDs istat format

--- overtourism/overtourism/backend_extension/api/v2/indexes.py
+++ overtourism/overtourism/backend_extension/api/v2/indexes.py
@@ -22,13 +22,17 @@ from overtourism.backend.api.v2.models.trentino_indicators import (
     CODICI_COMUNI_FILE,
     MACRO_AREAS_FILE,
     MAP_SHAPEFILE,
+    MAP_IDS,
+    _VODAFONE_ID_INDICATORS,
     get_indicator,
 )
 from overtourism.backend.api.v2.index_utils_trentino import (
     get_list_comuni,
     get_macro_areas,
     get_map_geometry,
+    get_vodafone_map_geometry,
     _build_macro_area_geodataframe,
+    _build_geodataframe_vodafone_ids
 )
 
 logger = logging.getLogger(__name__)
@@ -198,8 +202,10 @@ def get_index_data(
             f"[{index}] start_date={start_date}, end_date={end_date}, "
             f"extra={extra}, spatial_granularity={tc.spatial_granularity}"
         )
-
-        gdf_base = get_map_geometry(MAP_SHAPEFILE)
+        if index in _VODAFONE_ID_INDICATORS:
+            gdf_base = get_vodafone_map_geometry(MAP_IDS)   
+        else:
+            gdf_base = get_map_geometry(MAP_SHAPEFILE)
 
         computed_index = indicator.get_indicator(
             start_date=start_date,
@@ -215,6 +221,8 @@ def get_index_data(
                 gdf_final = _build_macro_area_geodataframe(
                     result, MACRO_AREAS_FILE, MAP_SHAPEFILE
                 )
+            elif index in _VODAFONE_ID_INDICATORS:
+                gdf_final = _build_geodataframe_vodafone_ids(gdf_base, result)
             else:
                 gdf_final = _build_geodataframe(gdf_base, result)
             geo_data = _to_map_response(gdf_final)
