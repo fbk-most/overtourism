bc4683467a7a58b7dc8c17838ddac584cde832cd
aricci <aricci@fbk.eu>
2026-08-28 17:42:11 +0200

    restore file, delete obsolete file

--- data_preparation/v2/main_gen_phenomena.py
+++
@@ -1,6 +0,0 @@
-from diffusion.main_generate_flows_and_grids import main_generate_flows_and_grids
-from gen_phenomenon_dataframes import main as generate_df
-
-if __name__ == "__main__":
-    main_generate_flows_and_grids(local = False)
-    generate_df()
