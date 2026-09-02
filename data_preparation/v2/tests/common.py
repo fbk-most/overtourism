c419b5d23db2a610ac58967f8d8775e9af528c33
aricci <aricci@fbk.eu>
2026-09-02 12:43:21 +0200

    add comuni -> IDs mapping creation

--- data_preparation/v2/tests/common.py
+++ data_preparation/v2/tests/common.py
@@ -8,6 +8,11 @@ def atp_to_comuni(json_apt):
             id_to_apt[id_6_cifre] = apt
     return id_to_apt
 
+def comuni_to_id(json_comuni):
+    return {
+        str(int(id_comune)).zfill(6): comune for comune, id_comune in json_comuni.items()
+    }
+
 
 def random_rgb():
     import secrets
@@ -15,3 +20,9 @@ def random_rgb():
     g = secrets.randbelow(256)
     b = secrets.randbelow(256)
     return (r, g, b)
+
+
+if __name__=="__main__":
+    from data_preparation.v2.utils.utils import get_json_s3
+    json_apt = get_json_s3("mapping_ids/map_comuni_into_apt.json")
+    json_comuni = get_json_s3("mapping_ids/mapping_comuni_ISTAT.json")
