2ce4dbeb420e3ecd3f234e29783323f9c5768e5b
aricci <aricci@fbk.eu>
2026-09-20 12:15:11 +0200

    add part for strutture 2025

--- data_preparation/v2/update_phenomenon_dataframes.py
+++ data_preparation/v2/update_phenomenon_dataframes.py
@@ -1,7 +1,6 @@
 """
 File dedicated to update the phenomena 
-The updating procedure shall include a STANDARDIZATION PART (to make the new phenomena compatible with the old ones) and an UPDATE PART (taking into account data overlaps, 
-which are for choice updated to the newest ones)
+The updating procedure shall include a STANDARDIZATION PART (to make the new phenomena compatible with the old ones) and an UPDATE PART (taking into account data overlaps, which are for choice updated to the newest ones)
 """
 
 import logging
@@ -17,7 +16,6 @@ import pandas as pd
 logging.basicConfig(level=logging.INFO)
 
 ## STANDARDIZATION OF NEW DATA 
-
 RENAMING_STRUTTURE = {
     "Esercizi alberghieri Numero": "alberghieri strutture",
     "Esercizi alberghieri Letti": "alberghieri posti_letto",
@@ -26,8 +24,10 @@ RENAMING_STRUTTURE = {
     "Totale Numero": "tot convenzionali strutture",
     "Totale Letti": "tot convenzionali posti_letto",
     "Alloggi turistici Numero": "all. privati numero",  # to check 
-    "Alloggi turistici Letti": "all. privati posti_letto",
+    'Alloggi a disposizione Numero': 'all.disposizione numero',
+    'Alloggi a disposizione Letti': 'all. disposizione posti_letto'
 }
+
 
 ## popolazione 
 def standardize_upd_popolazione_2025(df, mapping_comuni):
@@ -41,7 +41,7 @@ def standardize_upd_popolazione_2025(df, mapping_comuni):
 ## strutture annuario 
 
 def standardize_upd_strutture_2024(mapping_comuni):
-    """Adapts the structure to the "standard" one in order to reuse standardize_strutture()"""
+    """Adapts the strutture to the "standard" one in order to reuse standardize_strutture()"""
     logging.info("Downloading strutture_annuario_2024.csv from S3...")
     df = pd.read_excel(
         get_s3("strutture_annuario_2024.ods"),
@@ -50,6 +50,20 @@ def standardize_upd_strutture_2024(mapping_comuni):
     df = df.rename(columns=RENAMING_STRUTTURE)
     df["anno"] = 2024
     return standardize_strutture(df[["Comuni", "anno"] + list(RENAMING_STRUTTURE.values())], mapping_comuni, comune_col = "Comuni")
+
+
+def standardize_upd_strutture_2025(mapping_comuni):
+    """Adapts the strutture to the "standard" one in order to reuse standardize_strutture()"""
+    df = pd.read_excel(get_s3("numero_strutture_ISPAT_2025.xlsx"), header=[0, 1])
+
+    df.columns = [
+        f"{col[0]} {col[1]}".strip() if "Unnamed" not in str(col[1]) else col[0] 
+        for col in df.columns
+    ]
+    df = df.rename(columns=RENAMING_STRUTTURE)
+    df["anno"]=2025
+    return standardize_strutture(df[["Comune", "anno"] + list(RENAMING_STRUTTURE.values())], mapping_comuni, comune_col = "Comune")
+
 
 ## vodafone attendences 
 
@@ -65,6 +79,8 @@ def standardize_upd_data():
     logging.info("Downloading strutture_annuario_2024.csv'...")
     strutture_24_df = standardize_upd_strutture_2024(mapping_comuni)
     print(strutture_24_df.head())
+    strutture_25_df = standardize_upd_strutture_2025(mapping_comuni)
+    print(strutture_25_df.head())
 
 ## UPDATE OF PHENOMENA
 
